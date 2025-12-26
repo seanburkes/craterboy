1d199ba1d0a41a46d83dfc274b095c0638e5be73
Sean Burkes <sean.burkes@gmail.com>
2025-12-26 13:07:30 -0700

    Add initial unit test

--- src/domain/emulator.rs
+++ src/domain/emulator.rs
@@ -1,7 +1,26 @@
-pub struct Emulator;
+#[derive(Debug)]
+pub struct Emulator {
+    booted: bool,
+}
 
 impl Emulator {
     pub fn new() -> Self {
-        Self
+        Self { booted: false }
+    }
+
+    pub fn is_booted(&self) -> bool {
+        self.booted
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Emulator;
+
+    #[test]
+    fn new_emulator_starts_unbooted() {
+        let emulator = Emulator::new();
+
+        assert!(!emulator.is_booted());
     }
 }
